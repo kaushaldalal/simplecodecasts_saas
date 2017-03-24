cb6c36d43b305e9203684dc5f743bda1893c54c5
Kaushal Dalal <kaushal_dalal@yahoo.com>
2017-03-24 09:27:07 +0000

    Fixed typos

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -29,6 +29,7 @@ $(document).ready(function() {
         
         // Get the token from the response:
         var token = response.id;
+        alert(token);
         
         // Add the token to the form:
         f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
