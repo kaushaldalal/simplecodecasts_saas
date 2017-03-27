8326bf080a5b45a37c8090b236b923866dd29c50
Kaushal Dalal <kaushal_dalal@yahoo.com>
2017-03-27 11:33:00 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,15 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile
     end
     
+    def create
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+        
+    end
+    
+    
+    private 
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
 end
