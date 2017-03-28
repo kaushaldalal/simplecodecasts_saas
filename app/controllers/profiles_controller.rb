310ea1defa7509a1b0b511a3dd12032b3d11c87f
Kaushal Dalal <kaushal_dalal@yahoo.com>
2017-03-28 02:42:37 +0000

    Added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,12 +8,18 @@ class ProfilesController < ApplicationController
     def create
         @user = User.find( params[:user_id] )
         @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            render action: :new
+        end
         
     end
     
-    
-    private 
+    private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
         end
+    
 end
